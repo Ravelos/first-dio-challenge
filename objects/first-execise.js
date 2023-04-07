edbec70b4667996aec0e872d646106834db9fb06
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-06 23:33:30 -0300

    Creates classifyBmi function

--- objects/first-execise.js
+++ objects/first-execise.js
@@ -1,52 +1,62 @@
-
-
 /* This is a program for calculating the value spent on a trip by a car.
 We created a class and you can create more objects from that one and repeat the process
 You can run node first-exercise.js in the console to test it
  */
 
-class car{
-    brandName;
-    color;
-    gasConsumptionPerKm;
+class car {
+  brandName;
+  color;
+  gasConsumptionPerKm;
 
-    constructor(brandName,color,gasConsumptionPerKm){
-        this.brandName = brandName;
-        this.color = color;
-        this.gasConsumptionPerKm = gasConsumptionPerKm;
+  constructor(brandName, color, gasConsumptionPerKm) {
+    this.brandName = brandName;
+    this.color = color;
+    this.gasConsumptionPerKm = gasConsumptionPerKm;
+  }
 
-    }
-
-    gasExpenses(gasPrice,distanceKm){
-        return distanceKm * this.gasConsumptionPerKm * gasPrice;
-    }
+  gasExpenses(gasPrice, distanceKm) {
+    return distanceKm * this.gasConsumptionPerKm * gasPrice;
+  }
 }
 
-const volvo = new car('volvo','red', 3/12);
+const volvo = new car("volvo", "red", 3 / 12);
 
-console.log('You spent '+ volvo.gasExpenses(5,25) + '$Rs on this trip');
-
+console.log("You spent " + volvo.gasExpenses(5, 25) + "$Rs on this trip");
 
 /*
 Create a class person to calculate its BMI(Body Mass Index)
 */
 
-class person{
-    personName;
-    weightInKg;
-    heightInMeters;
+class person {
+  personName;
+  weightInKg;
+  heightInMeters;
 
-    constructor(personName,weightInKg,heightInMeters){
-        this.personName = personName;
-        this.weightInKg = weightInKg;
-        this.heightInMeters = heightInMeters;
-    }
+  constructor(personName, weightInKg, heightInMeters) {
+    this.personName = personName;
+    this.weightInKg = weightInKg;
+    this.heightInMeters = heightInMeters;
+  }
 
-    calculateBMI(){
-        return this.weightInKg / (this.heightInMeters * this.heightInMeters);
+  calculateBmi() {
+    return this.weightInKg / (this.heightInMeters * this.heightInMeters);
+  }
+
+  classifyBmi() {
+    const bmi = this.calculateBmi();
+    if (bmi < 18.5) {
+      return "You are underweight";
+    } else if (bmi < 25) {
+      return "You have a healthy weight";
+    } else if (bmi < 30) {
+      return "You are overweight";
+    } else {
+      return "You are in the obesity range";
     }
+  }
 }
 
-const robert = new person('Robert', 80 , 7/4);
+const robert = new person("Robert", 80, 7 / 4);
 
-console.log('Your BMI is: '+ robert.calculateBMI());
+console.log("Your BMI is: " + robert.calculateBmi());
+console.log(robert.classifyBmi());
