6ee2c89a720ccbcd4af77d63a203b0dcb97681a9
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-07 16:45:04 -0300

    Modifies the array

--- import/auxiliary-functions.js
+++ import/auxiliary-functions.js
@@ -1,14 +1,13 @@
-const inputs = [5, 50, 10, 98, 23];
+const inputs = [5, 50, 10, 98, 235, 53, 12, 90, 23];
 let i = 0;
 
 function gets() {
     const value = inputs[i];
     i++;
-    return value;
+    return value;    
 }
 
 function print(text) {
   console.log(text);
 }
-
 module.exports = { gets, print };
