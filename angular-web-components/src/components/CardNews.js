971fa3d7001e7a30d06539df5112e5906ed14918
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-18 13:00:06 -0300

    Makes the build of our component

--- angular-web-components/src/components/CardNews.js
+++ angular-web-components/src/components/CardNews.js
@@ -11,11 +11,26 @@ class CardNews extends HTMLElement{
         componetRoot.setAttribute("class","card");
         
         
+        // Creating card left
+        
         const cardLeft = document.createElement("div");
         cardLeft.setAttribute("class","card__left");
+
+        const author = document.createElement("span");
+        const linkTitle = document.createElement("a");
+        const newsContent = document.createElement("p");
+
+        cardLeft.appendChild(author);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        // Creating card right
         
         const cardRight = document.createElement("div");
         cardRight.setAttribute("class","card__right");
+
+        const newsImage = document.createElement("img");
+        cardRight.appendChild(newsImage)
 
         componetRoot.appendChild(cardLeft);
         componetRoot.appendChild(cardRight);
