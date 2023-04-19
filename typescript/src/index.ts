e0effa8fab5a8be542a7c31c47dcb64da9679c56
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-19 18:33:28 -0300

    Gives an example of attribute decorator

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,13 +1,39 @@
-type hero = {
-  name: string;
-  nick: string;
-};
+// Decorators
 
-function printObject(person: hero) {
-  console.log(person);
+function apiVersion(version: string){
+  return (target: any) => {
+    Object.assign(target.prototype, { __version: version, __name:"Oswaldo" });
+  }
+}
+function minLength(length: number){
+  return (target:any, key: string) => {
+    let _value = target[key];
+
+    const getter = () => _value;
+    const setter = (value:string) => {
+      if(value.length < length){
+        throw new Error(`Length is less than ${length}`);
+      }else{
+        _value = value;
+      }
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+    })
+  }
 }
 
-printObject({
-  name: "Bruce Wayne",
-  nick: "Batman",
-});
+@apiVersion("1.10")
+class Api{
+  @minLength(10)
+  name:string;
+
+  constructor(name:string){
+    this.name = name;
+  }
+}
+
+const api = new Api("products");
+console.log(api.name);
