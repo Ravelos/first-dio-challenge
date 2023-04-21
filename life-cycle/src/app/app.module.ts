8f767cc4b7829ed7c32e5e53a0e41f175d3a0c9c
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-21 16:53:24 -0300

    Declares title component

--- life-cycle/src/app/app.module.ts
+++ life-cycle/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
