a314ae0d3de8c11aac24cf898da75ef4e7235d86
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-22 20:53:06 -0300

    Creates attributes components

--- directives-proj/src/app/app.module.ts
+++ directives-proj/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
-import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
+import { CompAttributesComponent } from './comp-attributes/comp-attributes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CompAttributesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
