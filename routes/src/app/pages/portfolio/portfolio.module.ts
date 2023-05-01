2f46d42928c547925e14a4736036cff380f059e4
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-01 14:54:17 -0300

    First commit

---
+++ routes/src/app/pages/portfolio/portfolio.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardComponent
+  ],
+  exports:[CardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PortfolioModule { }
