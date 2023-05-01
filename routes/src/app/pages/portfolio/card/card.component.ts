7dc864c30580e12eb7ad246d9b9676c5276f2492
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-01 17:20:07 -0300

    Handles the parameters for the routes

--- routes/src/app/pages/portfolio/card/card.component.ts
+++ routes/src/app/pages/portfolio/card/card.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  constructor(private parameterizer: ActivatedRoute, private router: Router) {
+    // http://localhost:4200/portfolio/2
+    this.parameterizer.params.subscribe((res) => console.log(res));
 
-  constructor() { }
+    this.parameterizer.firstChild?.params.subscribe((res) => console.log(res));
 
-  ngOnInit(): void {
+    // http://localhost:4200/portfolio/2?name=oswaldo&token=123
+    this.parameterizer.queryParams.subscribe((res) => console.log(res));
   }
 
+  ngOnInit(): void {
+    // setInterval(() => {
+    //   this.router.navigate(['/']);
+    // }, 5000);
+  }
 }
