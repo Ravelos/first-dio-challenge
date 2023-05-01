fc586e318abf25f940b33f5a8c45ee3694fd74ec
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-01 17:17:45 -0300

    Imports the modules

--- routes/src/app/app.module.ts
+++ routes/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IndexModule } from './pages/index/index.module';
+import { PortfolioModule } from './pages/portfolio/portfolio.module';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IndexModule,
+    PortfolioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
