089a28f4c72f401203524885dd16d6630ccf2d11
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-01 17:16:46 -0300

    Creates the routes

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portfolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TitleComponent, pathMatch: 'full' },
+  {
+    path: 'portfolio',
+    component: CardComponent,
+    children: [
+      { path: ':id', component: CardComponent },
+      { path: ':id/:token', component: CardComponent },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
