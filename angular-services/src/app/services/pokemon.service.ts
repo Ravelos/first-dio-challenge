f6c379fb3fe5454326b0b1d260a0d7e40c3cc1ef
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-03 19:54:08 -0300

    Creates pokemon service

---
+++ angular-services/src/app/services/pokemon.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  private baseUrl: string = '';
+  private pokeData: any;
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = environment.pokeApi;
+  }
+
+  getPokemonName(pokemonName: string) {
+    this.pokeData = this.http.get(`${this.baseUrl}${pokemonName}`);
+  }
+}
