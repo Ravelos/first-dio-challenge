f3322c3eea756dba49a16007f4ac1dfdd729523c
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-02 21:39:38 -0300

    Places the html on this file

--- angular-services/src/app/app.component.ts
+++ angular-services/src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Services in angular</h1>
+  <app-card></app-card>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
