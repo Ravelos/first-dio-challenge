fd70ac2faa1af5014b312b64370068c1e0c0b8b6
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-03 22:28:55 -0300

    Implements the service

--- angular-services/src/app/components/card/card.component.ts
+++ angular-services/src/app/components/card/card.component.ts
@@ -1,22 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { debounce } from 'rxjs';
+import { PokemonData } from 'src/app/models/pokemonData';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  pokemon: PokemonData;
 
-  name:string = "Charizard";
-  attributeTypes: string[] = ["FIRE", "ROCK"];
+  constructor(private service: PokemonService) {
+    this.pokemon = {
+      id: 0,
+      name: '',
+      sprites: {
+        front_default: '',
+      },
+      types:[]
+      }
+    }
 
-  constructor(
-    private service: PokemonService
-  ) { }
 
   ngOnInit(): void {
-    this.service.getPokemonName("Balbasaur");
+    this.getPokemon('pikachu')
+  }
+
+  getPokemon(searchName:string){
+    this.service.getPokemonName(searchName).subscribe({
+      next: (res) => {
+        this.pokemon = {
+          id: res.id,
+          name: res.name,
+          sprites: res.sprites.front_default,
+          types: res.types,
+        };
+
+        console.log(this.pokemon);
+      },
+      error: (err) => console.log('NOT FOUND'),
+    }), debounce;
   }
 
 }
