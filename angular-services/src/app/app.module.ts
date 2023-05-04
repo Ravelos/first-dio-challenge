fe61d1dfd153a051fdcedf5ab4f9e3484f272d1b
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-03 22:26:26 -0300

    Imports forms module

--- angular-services/src/app/app.module.ts
+++ angular-services/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { CardComponent } from './components/card/card.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
