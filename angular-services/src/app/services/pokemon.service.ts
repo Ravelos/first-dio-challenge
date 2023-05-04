ab0fc69e85b53d8735e2a973ec3002e721ae4edf
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-03 22:29:43 -0300

    Cretes the method or the service

--- angular-services/src/app/services/pokemon.service.ts
+++ angular-services/src/app/services/pokemon.service.ts
@@ -1,19 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PokemonData } from '../models/pokemonData';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
   private baseUrl: string = '';
-  private pokeData: any;
+  private pokeData: PokemonData | any;
 
   constructor(private http: HttpClient) {
     this.baseUrl = environment.pokeApi;
   }
 
-  getPokemonName(pokemonName: string) {
-    this.pokeData = this.http.get(`${this.baseUrl}${pokemonName}`);
+  getPokemonName(pokemonName: string): Observable<PokemonData | any> {
+    this.pokeData = this.http.get<PokemonData>(`${this.baseUrl}${pokemonName}`);
+    return this.pokeData;
   }
 }
