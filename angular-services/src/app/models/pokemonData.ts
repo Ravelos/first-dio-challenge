18544654a83ee50acb83b63d7b15ee06d8bd5f57
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-03 22:25:41 -0300

    Creates the model

---
+++ angular-services/src/app/models/pokemonData.ts
@@ -0,0 +1,14 @@
+export type PokemonData = {
+  id:number;
+  name:string;
+  sprites:{
+    front_default:string,
+  };
+  types:{
+    slot: number
+    type: {
+      name: string
+      url:string
+    }
+  }[]
+}
